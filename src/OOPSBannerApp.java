ae2f654937625d87aa1c06d569f2305b58ce5252
chintala-saikrishna <chintalasaikrishna2007@gmail.com>
2026-04-14 00:06:42 +0530

    UC3: Modularized code using methods

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,13 +1,42 @@
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
+        printO();
+        System.out.print("   ");
+        printO();
+        System.out.print("   ");
+        printP();
+        System.out.print("   ");
+        printS();
+    }
 
-        System.out.println("*****   *****   *****   *****");
-        System.out.println("*   *   *   *   *   *   *");
-        System.out.println("*   *   *   *   *   *   *");
-        System.out.println("*   *   *****   *****   *****");
-        System.out.println("*   *   *       *           *");
-        System.out.println("*   *   *       *           *");
-        System.out.println("*****   *       *****   *****");
+    public static void printO() {
+        System.out.println("*****");
+        System.out.println("*   *");
+        System.out.println("*   *");
+        System.out.println("*   *");
+        System.out.println("*   *");
+        System.out.println("*   *");
+        System.out.println("*****");
+    }
 
+    public static void printP() {
+        System.out.println("*****");
+        System.out.println("*   *");
+        System.out.println("*   *");
+        System.out.println("*****");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+    }
+
+    public static void printS() {
+        System.out.println("*****");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*****");
+        System.out.println("    *");
+        System.out.println("    *");
+        System.out.println("*****");
     }
 }
