24f04d43958001a2e60360202a808101f48a3775
chintala-saikrishna <chintalasaikrishna2007@gmail.com>
2026-04-14 00:15:52 +0530

    UC4: Separate class for patterns

---
+++ src/CharacterPatterns.java
@@ -0,0 +1,32 @@
+public class CharacterPatterns {
+
+    public void printO() {
+        System.out.println("*****");
+        System.out.println("*   *");
+        System.out.println("*   *");
+        System.out.println("*   *");
+        System.out.println("*   *");
+        System.out.println("*   *");
+        System.out.println("*****");
+    }
+
+    public void printP() {
+        System.out.println("*****");
+        System.out.println("*   *");
+        System.out.println("*   *");
+        System.out.println("*****");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+    }
+
+    public void printS() {
+        System.out.println("*****");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*****");
+        System.out.println("    *");
+        System.out.println("    *");
+        System.out.println("*****");
+    }
+}
--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,42 +1,15 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        printO();
-        System.out.print("   ");
-        printO();
-        System.out.print("   ");
-        printP();
-        System.out.print("   ");
-        printS();
-    }
 
-    public static void printO() {
-        System.out.println("*****");
-        System.out.println("*   *");
-        System.out.println("*   *");
-        System.out.println("*   *");
-        System.out.println("*   *");
-        System.out.println("*   *");
-        System.out.println("*****");
-    }
+        CharacterPatterns cp = new CharacterPatterns();
 
-    public static void printP() {
-        System.out.println("*****");
-        System.out.println("*   *");
-        System.out.println("*   *");
-        System.out.println("*****");
-        System.out.println("*");
-        System.out.println("*");
-        System.out.println("*");
-    }
-
-    public static void printS() {
-        System.out.println("*****");
-        System.out.println("*");
-        System.out.println("*");
-        System.out.println("*****");
-        System.out.println("    *");
-        System.out.println("    *");
-        System.out.println("*****");
+        cp.printO();
+        System.out.print("   ");
+        cp.printO();
+        System.out.print("   ");
+        cp.printP();
+        System.out.print("   ");
+        cp.printS();
     }
 }
