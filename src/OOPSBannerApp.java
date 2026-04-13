6aa97b6a5ffe3b550cfc8e0f8387b32e7fcddd63
chintala-saikrishna <chintalasaikrishna2007@gmail.com>
2026-04-14 00:34:51 +0530

    UC7: Dynamic input using Scanner

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,17 +1,30 @@
+import java.util.Scanner;
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
         CharacterPatterns cp = new CharacterPatterns();
+        Scanner sc = new Scanner(System.in);
 
-        char[] word = {'O', 'O', 'P', 'S'};
+        System.out.print("Enter text: ");
+        String input = sc.nextLine().toUpperCase();
+
+        char[] word = input.toCharArray();
 
         for (int i = 0; i < 7; i++) {
             for (char ch : word) {
                 String[] pattern = cp.getPattern(ch);
-                System.out.print(pattern[i] + "   ");
+
+                if (pattern != null) {
+                    System.out.print(pattern[i] + "   ");
+                } else {
+                    System.out.print("       "); // for unknown chars
+                }
             }
             System.out.println();
         }
+
+        sc.close();
     }
 }
